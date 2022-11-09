b3ddff78ef72ea32f8ae28384ced08ef192fbb35
Eric Shrestha <63570381+Chewbacapaca@users.noreply.github.com>
2022-11-09 10:53:08 +0545

    removed redundant folders

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const Product = require("../model/productModel");
+const Product = require("../models/productModel");
 
 router.post("/product/add",(req,res)=>{
     const price = req.body.price;
