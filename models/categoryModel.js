cc4dc0393382a78a3a4dc88d4129bf4648200258
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-11-16 15:40:47 +0545

    category add image

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const category = new mongoose.Schema({
     name:{
         type:String
+    },
+    image:{
+        type:String
     }
 })
 
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = new express.Router();
 const Category = require("../models/categoryModel");
+const multer = require("multer");
+const upload = require("../fileupload/fileupload");
 
-router.post("/category/add",(req,res)=>{
+router.post("/category/add",upload.single("category_img"),(req,res)=>{
     const name = req.body.name;
+    const image = req.file.filename;
 
     const data = new Category({
         name: name,
+        image: image
+
     });
     data.save()
     .then(()=>{
@@ -18,6 +23,18 @@ router.post("/category/add",(req,res)=>{
         res
         .status(402)
         .json({e})
+    })
+})
+
+router.get("/category/show",(req,res)=>{
+    Category.find()
+    .then((data)=>{
+        res
+        .json({success:true,data:data})
+        
+    }).catch((e)=>{
+        res.status(404)
+        .json({error:e})
     })
 })
 
