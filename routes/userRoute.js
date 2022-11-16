22ccc8347ec11412059ac0f2ce0e9cd1f9486be4
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-11-16 16:15:11 +0545

    userprofile - show user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,12 +2,21 @@ const express = require("express");
 const { registerUser, loginUser, logout } = require("../controllers/userController");
 const multer = require("multer")
 const router = express.Router();
-
+const User = require("../models/userModel");
 const upload = multer();
 
 router.route("/register").post(upload.none(), registerUser)
 
 router.route("/login").post(loginUser)
+
+router.get("/show",(req,res)=>{
+    User.find()
+    .then((data)=>{
+        res.json({data:data})
+    }).catch((e)=>{
+        res.json({error:e})
+    })
+})
 
 router.route("/logout").get(logout)
 
