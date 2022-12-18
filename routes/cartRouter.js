439f9fa2b8942c3d734efd7552f2b67ce92f1914
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-12-18 12:03:34 +0545

    cart no duplicate

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -3,36 +3,63 @@ const router = new express.Router();
 const Cart = require("../models/cartModel");
 const {isAuthenticatedUser} = require("../middleware/auth");
 
-router.post("/cart/add",isAuthenticatedUser,(req,res)=>{
+router.post("/cart/add",isAuthenticatedUser,async (req,res)=>{
     const user_name = req.user.id;
     const added_product = req.body.added_product;
-    if(Cart.findOne({user_name:user_name})){
-        const data = new Cart({
-            user_name : user_name
-        })
-        data.save()
+
+    
+    let c;
+    
+    try{
+        c = await Cart.findOne({user_name:user_name});
+        
+    }catch(e){
+        console.log(e);
+    }
+    if(c == null){
+        let repeat_product;
+    }
+    else{
+        for(let i=0; i<c.products.length; i++){
+            let loop_product=c.products[i].added_product;
+            if(added_product == loop_product){
+                var repeat_product=loop_product;
+            }
+        }
     }
     
-    Cart.findOneAndUpdate({user_name:user_name},
-        {
-            $addToSet:{
-                products:[
-                {
-                    added_product:added_product
-                }
-            ]}
-        })
-        .then(()=>{
-            res
-            .json({success:true,message:"Added to cart"})
-            .status(201)
-        })
-        .catch((e)=>{
-            res.json({success:false,error:e})
-        })
     
+    if(repeat_product == null){
+        Cart.findOneAndUpdate({user_name:user_name},
+            {
+                $addToSet:{
+                    products:[
+                    {
+                        added_product:added_product
+                    }
+                ]}
+            },{ upsert: true, new: true })
+            .then(()=>{
+                res
+                .json({success:true,message:"Added to cart"})
+                .status(201)
+            })
+            .catch((e)=>{
+                res.json({success:false,error:e})
+            })   
+    }
+    else{
+        res.json({success:true,msg:"already exists"})
+    }
+    
+  
+    
+            
+        
     
         
+    
+      
 })
 
 router.get("/cart/show",isAuthenticatedUser,(req,res)=>{
